843e86459f30836ec5735d41afbd19bcb2a0e739
princeghoshc7 <pk.121ghosh@gmail.com>
2025-01-01 19:39:21 +0530

    Array_demo

---
+++ DailyCodingProblem/src/Array/arrayDemo.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class arrayDemo {
+
+	public static void main(String[] args) {
+		
+		int[] array=new int[]{1,2,3,4};	
+	// TODO Auto-generated method stub
+		for (int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+
+	}
+
+}
